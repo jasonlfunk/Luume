3cbf1ef0330f9be368844d4ae13c2f00d4e424d5
Jason Funk <jasonlfunk@gmail.com>
2012-03-10 15:57:29 -0600

    Add task views

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,73 @@
+class TasksController < ApplicationController
+  before_filter :login_required
+
+  def index
+    @project = Project.find(params[:project_id])
+    @tasks = @project.tasks
+  end
+  
+  def new
+    @project = Project.find(params[:project_id])
+    @task = @project.tasks.build
+  end
+
+  # GET /tasks/new
+  # GET /tasks/new.json
+  def new
+    @task = Task.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @task }
+    end
+  end
+
+  # GET /tasks/1/edit
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # POST /tasks
+  # POST /tasks.json
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to @task, :notice => 'Task was successfully created.' }
+        format.json { render :json => @task, :status => :created, :location => @task }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @task.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tasks/1
+  # PUT /tasks/1.json
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update_attributes(params[:task])
+        format.html { redirect_to @task, :notice => 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @task.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.json
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.json { head :no_content }
+    end
+  end
+end
